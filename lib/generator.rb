7fcacb44952f890f1f6b3c1c9682d68c5a9a4523
Yoshihide Chubachi <yoshi@chubachi.net>
2013-08-07 22:51:31 +0900

    Refactoring

--- lib/generator.rb
+++ lib/generator.rb
@@ -17,6 +17,8 @@ class Generator
   Qwerty = C鍵盤::Qwerty
   Dvorak = C鍵盤::Dvorak
 
+  C省略 = nil
+
   # DSL実行
   # DSL初期化
   def initialize
@@ -82,11 +84,11 @@ class Generator
 
     結果 = []
     @二重母音.each do |二重母音|
-      段 = @五十音.表[:あ行].index(二重母音[0])
-      if 段 == nil
+      列 = @五十音.表[:あ行].index(二重母音[0])
+      if 列 == C省略
         raise "「#{二重母音[0]}」はあ行の文字ではありません"
       end
-      結果 << "#{行[段]}#{二重母音[1]}"
+      結果 << "#{行[列]}#{二重母音[1]}"
     end
     結果
   end
@@ -123,54 +125,23 @@ class Generator
   #   いずれの場合も，必ず「かな」と同じ長さになるようにすること．
   # @return [Array] ローマ字変換列
   # @todo 中間鍵を配列でも渡せるようにする
-  def 変換(かな, 追加: '', 開始鍵: nil, 中間鍵: nil, 確定鍵: nil)
-    # 引数検査
+  def 変換(文字, 追加文字: '', 開始鍵: nil, 中間鍵: nil, 確定鍵: nil)
     case
     when 開始鍵 && 開始鍵.is_a?(Hash) == false
       raise '開始鍵は連想配列で指定，または，省略してください'
     when 中間鍵 && 中間鍵.is_a?(Hash) == false
       raise '中間鍵は連想配列で指定，または，省略してください'
+    when 確定鍵 && (確定鍵.is_a?(Hash) == false && 確定鍵.is_a?(Array) == false)
+      raise '確定鍵は連想配列または配列で指定，または，省略してください'
     end
 
-    かな配列 = かな正規化(かな)
-    確定鍵配列 =
-      case 確定鍵
-      when Array
-        # Making Deep Copies in Ruby
-        # - http://ruby.about.com/od/advancedruby/a/deepcopy.htm)
-        Marshal.load(Marshal.dump(確定鍵))
-      when Hash
-        case
-        when 確定鍵[:番号]
-          if (0..4).include?(確定鍵[:番号])
-          else
-            raise '確定鍵の番号は[0..4]で指定してください'
-          end
-          [確定鍵]
-        else
-          if @母音順
-          else
-            raise '確定鍵の番号を省略する場合は母音順を設定してください'
-          end
-          結果 = []
-          for 番号 in 0..4
-            結果 << {左右: 確定鍵[:左右], 段: 確定鍵[:段], 番号: @母音順[番号]}
-          end
-          結果
-        end
-      when nil
-        鍵盤確定鍵
-      else
-        raise '確定鍵位置は配列，連想配列またはnilで指定してください'
-      end
-
-    if かな配列.length != 確定鍵配列.length
-      raise 'かなは確定鍵と同じ文字数で指定してください'
+    文字配列   = 文字正規化(文字)
+    確定鍵配列 = 確定鍵正規化(確定鍵)
+    if 文字配列.length != 確定鍵配列.length
+      raise '文字は確定鍵と同じ文字数で指定してください'
     end
 
-    # 中間鍵の段を省略する場合
-    if 中間鍵 && 中間鍵[:段] == nil
-      中間鍵 = 中間鍵.dup
+    if 中間鍵 && 中間鍵[:段] == C省略
       中間鍵[:段] = 開始鍵[:段]
     end
 
@@ -183,13 +154,13 @@ class Generator
     end
 
     結果 = []
-    [かな配列, 確定鍵配列].transpose.each do | (かなI, 確定鍵I) |
-      # 確定鍵の段を省略する場合
-      if 確定鍵I[:段] == nil
+    [文字配列, 確定鍵配列].transpose.each do | (文字I, 確定鍵I) |
+      if 確定鍵I[:段] == C省略
         確定鍵I[:段] = 開始鍵[:段]
       end
       確定鍵R = @鍵盤[確定鍵I[:左右]][確定鍵I[:段]][確定鍵I[:番号]]
-      結果 << 変換表作成("#{開始鍵R}#{中間鍵R}#{確定鍵R}", "#{かなI}#{追加}")
+      結果 << 変換表作成("#{開始鍵R}#{中間鍵R}#{確定鍵R}",
+                         "#{文字I}#{追加文字}")
     end
     結果
   end
@@ -208,24 +179,53 @@ class Generator
   #        配列の場合は，そのまま返す．
   #        文字列の場合は，1文字ごとの配列にする．
   # @return [Array] かなの配列
-  def かな正規化(かな)
-    case かな
+  def 文字正規化(文字)
+    case 文字
     when Array
-      かな
+      文字
     when String
-      かな.split("")
+      文字.split("")
     when Symbol
-      @五十音.表[かな]
+      @五十音.表[文字]
     else
-      raise 'かなはシンボル，文字列，配列で指定してください'
+      raise '文字はシンボル，文字列，配列で指定してください'
     end
   end
 
-  # 母音位置にて番号が省略されている場合，登録されている母音順に従い位置の配列に正規化する
+  # TODO: 母音位置にて番号が省略されている場合，登録されている母音順に従い位置の配列に正規化する
   #
   # 番号を省略する場合は，必ず{#母音順}を設定しておくこと．
   #
   # @return [Array] 母音の配列
+  def 確定鍵正規化(確定鍵)
+    case 確定鍵
+    when Array
+      # Making Deep Copies in Ruby
+      # - http://ruby.about.com/od/advancedruby/a/deepcopy.htm)
+      Marshal.load(Marshal.dump(確定鍵))
+    when Hash
+      case
+      when 確定鍵[:番号]
+        if (0..4).include?(確定鍵[:番号])
+        else
+          raise '確定鍵の番号は[0..4]で指定してください'
+        end
+        [確定鍵]
+      else
+        if @母音順
+        else
+          raise '確定鍵の番号を省略する場合は母音順を設定してください'
+        end
+        結果 = []
+        for 番号 in 0..4
+          結果 << {左右: 確定鍵[:左右], 段: 確定鍵[:段], 番号: @母音順[番号]}
+        end
+        結果
+      end
+    when nil
+      鍵盤確定鍵
+    end
+  end
 
   def self.execute(contents)
     g = Generator.new
