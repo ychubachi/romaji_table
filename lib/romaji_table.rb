9e55ce075883ccc7c35ffc793c331df75b78cd03
Yoshihide Chubachi <yoshi@chubachi.net>
2013-08-08 20:50:34 +0900

    DSL

--- lib/romaji_table.rb
+++ lib/romaji_table.rb
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
 require "romaji_table/version"
+require "romaji_table/c生成器"
 
 module RomajiTable
   module Delegator
     def self.delegate(*methods)
       methods.each do |method_name|
         eval <<-RUBY, binding, '(__DELEGATE__)', 1
+          def #{method_name}(*args, &b)
+            ::RomajiTable::C生成器.
+              instance.send(#{method_name.inspect}, *args, &b)
+          end
+          # private #{method_name.inspect}
         RUBY
       end
     end
+
+    delegate :母音順
   end
 end
 
