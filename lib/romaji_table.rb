2cf0072bd44105d3a5be88441e8c2b889198c72f
Yoshihide Chubachi <yoshi@chubachi.net>
2013-08-09 00:59:30 +0900

    Enable to use DSL when require 'romaji_table'

--- lib/romaji_table.rb
+++ lib/romaji_table.rb
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-require "romaji_table/version"
-require "romaji_table/c生成器"
+require_relative "romaji_table/version"
+require_relative "romaji_table/c生成器"
 
 module RomajiTable
   module Delegator
     def self.delegate(*methods)
       methods.each do |method_name|
-        eval <<-RUBY, binding, '(__DELEGATE__)', 1
+        eval <<-RUBY, TOPLEVEL_BINDING, '(__DELEGATE__)', 1
           def #{method_name}(*args, &b)
             ::RomajiTable::C生成器.
               instance.send(#{method_name.inspect}, *args, &b)
@@ -15,10 +15,10 @@ module RomajiTable
         RUBY
       end
     end
-
     delegate :母音順
   end
 end
+
 
 # SinatraはDSLなんかじゃない、Ruby偽装を使ったマインドコントロールだ！
 # - hp12c - http://d.hatena.ne.jp/keyesberry/20110603/p1
